95930a173f2c6cefb5c3049e17b0cdb5f5332b74
lvlaciel <96140694+lvlaciel@users.noreply.github.com>
2022-11-01 17:55:18 -0300

    try

--- api/src/app.js
+++ api/src/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const cors = require('cors');
 
 require('./db.js');
 
 const server = express();
-const cors = require('cors');
 
 server.name = 'API';
+server.use(express.urlencoded({ extended: true, limit: "50mb" })); //middleware
+server.use(express.json({ limit: "50mb" }));
+server.use(cookieParser());
+server.use(morgan("dev"));
 
 server.use(
   cors({
@@ -26,10 +29,7 @@ server.use(
     ],
   })
 );
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
-server.use(morgan('dev'));
+
 // server.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', 'https://pi-food-two.vercel.app'); 
 //   res.header('Access-Control-Allow-Credentials', 'true');
